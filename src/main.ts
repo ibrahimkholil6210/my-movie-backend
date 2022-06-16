7f9ae17b89052b31d89720d66b997fe2ebfc93df
Ibrahim Kholil <33357137+ibrahimkholil6210@users.noreply.github.com>
2022-06-16 17:19:36 +0600

    intialize nest project

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(4000, () => console.log('Server is running on port 4000'));
+}
+bootstrap();
